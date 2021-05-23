0567de78d629e986ff87994282cc577e1f9343b8
Lasantha Bandara <lasantha.bandara@smithsdetection.com>
2021-05-24 05:33:00 +0800

    schema generation and data seeding

---
+++ src/main/resources/data-dev.sql
@@ -0,0 +1,4 @@
+insert into simpleweb.public.todo(description,details,done) values ('item 1','complete item 1',FALSE)
+insert into simpleweb.public.todo(description,details,done) values ('item 2','complete item 2',FALSE)
+insert into simpleweb.public.todo(description,details,done) values ('item 3','complete item 3',FALSE)
+insert into simpleweb.public.todo(description,details,done) values ('item 4','complete item 4',FALSE)
