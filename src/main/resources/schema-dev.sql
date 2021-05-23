aa36e3c4a7ad6d391a00f06b801d2bf2b335da1a
Lasantha Bandara <lasantha.bandara@smithsdetection.com>
2021-05-24 07:53:58 +0800

    creating the post method

--- src/main/resources/schema-dev.sql
+++ src/main/resources/schema-dev.sql
@@ -1,2 +1,9 @@
 DROP TABLE IF EXISTS todo;
 CREATE TABLE todo (id SERIAL PRIMARY KEY, description VARCHAR(255), details VARCHAR(4096), done BOOLEAN);
+
+create Sequence If NOT EXISTS HIBERNATE_SEQUENCE
+minvalue 100000
+maxvalue 9999999999999999
+start with 100060
+increment by 1
+cache 20;
